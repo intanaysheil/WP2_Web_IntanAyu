8251f8be0228a05ef162cf3dd33c8bcee14e6535
Intan Ayu .N <119681780+intanaysheil@users.noreply.github.com>
2023-05-05 01:40:22 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4> Nama </h4>
+    <ul type="disc">
+        <li> Nama Depan : Intan Ayu </li>
+        <li> Nama Belakang : Nursheila </li>
+    </ul>
+
+    <h4> Alamat </h4>
+    <ul type="none">
+        <li> Jalan Kebon Nanas Selatan, Jatinegara, Jakarta Timur </li>
+    </ul>
+
+    <h4> Tempat Lahir </h4>
+    <ul type="none">
+        <li> Kediri, Jawa Timur </li>
+    </ul>
+
+    <h4> Olah Raga Favorit </h4>
+    <ul type="square">
+        <li> Lari </li>
+        <li> Badminton </li>
+    </ul>
+</section>
